21a54f902c6f306b4f3901c4b7642aff8a19fe5f
Bernard Faria <bernardxf@gmail.com>
2018-01-26 11:21:51 -0200

    Better State Management with Vuex

---
+++ vuex/src/store/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import counter from './modules/counter';
+
+import actions from './actions';
+import mutations from './mutations';
+import getters from './getters';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+	state: {
+		value: 0
+	},
+	getters,
+	mutations,
+	actions,
+	modules: {
+		counter
+	}
+});
