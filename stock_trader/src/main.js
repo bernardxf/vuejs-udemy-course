b0c0e66a65e5cb644fc6a9404147ff441cd5e222
Bernard Faria <bernardxf@gmail.com>
2018-01-29 11:18:26 -0200

    Final Project - The Stock Trader

--- stock_trader/src/main.js
+++ stock_trader/src/main.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
 import App from './App.vue'
 import { routes } from './routes';
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-cd17f.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
 
 const router = new VueRouter({
-	mode: 'history',
-	routes
+  mode: 'history',
+  routes
 });
 
 new Vue({
