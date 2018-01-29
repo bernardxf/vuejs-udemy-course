229a43825201edad82e28869543e76b98cc23fd3
Bernard Faria <bernardxf@gmail.com>
2018-01-29 14:38:11 -0200

    Using Axios instead of vue-resource

---
+++ axios/src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+import router from './router'
+import store from './store'
+
+axios.defaults.baseURL = 'https://vuejs-axios-85f8f.firebaseio.com';
+axios.defaults.headers.common['Authorization'] = 'fasfdsa';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+	console.log('Request interceptor', config);
+	return config
+});
+const resInterceptor = axios.interceptors.response.use(res => {
+	console.log('Response interceptor', res);
+	return res
+});
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.request.eject(resInterceptor);
+
+new Vue({
+	el: '#app',
+	router,
+	store,
+	render: h => h(App)
+})
