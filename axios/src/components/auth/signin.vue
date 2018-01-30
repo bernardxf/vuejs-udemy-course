29f370b4b8a4066747c1ea67ef145f704735c8e9
Bernard Faria <bernardxf@gmail.com>
2018-01-30 15:18:47 -0200

    Form Input Validation

--- axios/src/components/auth/signin.vue
+++ axios/src/components/auth/signin.vue
@@ -4,17 +4,11 @@
 			<form @submit.prevent="onSubmit">
 				<div class="input">
 					<label for="email">Mail</label>
-					<input
-						type="email"
-						id="email"
-						v-model="email">
+					<input type="email" id="email" v-model="email">
 				</div>
 				<div class="input">
 					<label for="password">Password</label>
-					<input
-						type="password"
-						id="password"
-						v-model="password">
+					<input type="password" id="password" v-model="password">
 				</div>
 				<div class="submit">
 					<button type="submit">Submit</button>
